f52a137a7f94558bfdd03e5d11ba5d3552241ab0
[Andres] <andresar11@gmail.com>
2018-10-19 19:50:55 -0400

    Added basic nested routes to Topics component, will need to make routes more dynamic using URL parameter

--- TMREACT/Day4/test-app/src/components/Topics.js
+++ TMREACT/Day4/test-app/src/components/Topics.js
@@ -1,5 +1,39 @@
 import React from 'react'
+import {
+  Link,
+  Route
+ } from 'react-router-dom'
 
-export default function Topic() {
-  return <div> Topics </div>
+
+
+function Topic() {
+  return <h3>TOPIC 1</h3>
+}
+
+
+
+export default function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to='/topics/rendering'> Rendering With React</Link>
+        </li>
+        <li>
+          <Link to='/topics/components'>Components</Link>
+        </li>
+        <li>
+          <Link to='/topics/props-v-state'>Props vs State</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route path="/topics/rendering" component={Topic} />
+      <Route path="/topics/components" component={Topic} />
+      <Route path="/topics/props-v-state" component={Topic} />
+
+    </div>
+  )
 }
