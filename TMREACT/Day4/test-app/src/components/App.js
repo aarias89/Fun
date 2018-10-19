3b2ecae7e7693b02fc59d979028746ff6e4e3f23
[Andres] <andresar11@gmail.com>
2018-10-19 19:32:29 -0400

    Created app with basic routing using 3 different components

--- TMREACT/Day4/test-app/src/components/App.js
+++ TMREACT/Day4/test-app/src/components/App.js
@@ -2,18 +2,33 @@ import React, { Component } from 'react';
 import Home from './Home'
 import About from './About'
 import Topic from './Topics'
+import {
+  BrowserRouter,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Home />
-        <About />
-        <Topic />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/topics'>Topics</Link></li>
+          </ul>
+
+          <hr />
+
+          <Route exact path='/' component ={Home} />
+          <Route path='/about' component ={About} />
+          <Route path='/topics' component ={Topic} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
 
 export default App;
-// nmp install react-router
+//npm install react-rounter-dom
