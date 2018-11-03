03cea80b00e4271b6f69f3e995ef6035bf457a65
[Andres] <andresar11@gmail.com>
2018-11-03 15:31:34 -0400

    Using stateless functional components, no babel

---
+++ BHReact/PartI/src/App.js
@@ -0,0 +1,32 @@
+const Pet = (props) => {
+  return React.createElement("div",null,[
+    React.createElement("h1",{},props.name),
+    React.createElement("h2",{},props.animal),
+    React.createElement("h2",{},props.breed)
+  ])
+}
+
+
+
+const App = () => {
+  return React.createElement("div", {},[
+    React.createElement("h1", {}, 'Adopt Me!'),
+    React.createElement(Pet, {
+      name: 'Ricky',
+      animal: 'Dog',
+      breed: 'GSD'
+    }),
+    React.createElement(Pet, {
+      name: 'Katrina',
+      animal: 'Dog',
+      breed: 'GSD'
+    }),
+    React.createElement(Pet, {
+      name: 'Tango',
+      animal: 'Dog',
+      breed: 'Golden Retriver'
+    }),
+  ])
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'))
