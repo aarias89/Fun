26ada08e5278784c99a7478d7c3520a6f8fde015
[Andres] <andresar11@gmail.com>
2018-11-03 15:35:42 -0400

    Convert functional app component into class component

--- BHReact/PartI/src/App.js
+++ BHReact/PartI/src/App.js
@@ -6,27 +6,31 @@ const Pet = (props) => {
   ])
 }
 
+class App extends React.Component {
+  render () {
+    return React.createElement("div", {},[
+      React.createElement("h1", {}, 'Adopt Me!'),
+      React.createElement(Pet, {
+        name: 'Ricky',
+        animal: 'Dog',
+        breed: 'GSD'
+      }),
+      React.createElement(Pet, {
+        name: 'Katrina',
+        animal: 'Dog',
+        breed: 'GSD'
+      }),
+      React.createElement(Pet, {
+        name: 'Tango',
+        animal: 'Dog',
+        breed: 'Golden Retriver'
+      }),
+    ])
 
-
-const App = () => {
-  return React.createElement("div", {},[
-    React.createElement("h1", {}, 'Adopt Me!'),
-    React.createElement(Pet, {
-      name: 'Ricky',
-      animal: 'Dog',
-      breed: 'GSD'
-    }),
-    React.createElement(Pet, {
-      name: 'Katrina',
-      animal: 'Dog',
-      breed: 'GSD'
-    }),
-    React.createElement(Pet, {
-      name: 'Tango',
-      animal: 'Dog',
-      breed: 'Golden Retriver'
-    }),
-  ])
+  }
 }
+
+
+
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'))
