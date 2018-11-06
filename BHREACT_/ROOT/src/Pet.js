d5eaa48d740655aa1d6f10a50c8616f6f43ba772
[Andres] <andresar11@gmail.com>
2018-11-06 14:14:35 -0500

    Update props for Pet component, connected pet component to basic CSS

--- BHREACT_/ROOT/src/Pet.js
+++ BHREACT_/ROOT/src/Pet.js
@@ -1,13 +1,29 @@
 import React from "react";
 
-const Pet = props => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
-  );
-};
+export default class Pet extends React.Component {
+  render() {
 
-export default Pet;
+      const { name, animal, breed, media, location } = this.props;
+
+      let photos = []
+
+      if (media && media.photos && media.photos.photo) {
+        photos = media.photos.photo.filter( photo => photo["@size"]=== 'pn')
+      }
+
+      return (
+        <div className="pet">
+          <div className="image-container">
+            <img src={photos[0].value} alt={name}/>
+          </div>
+          <div className="info">
+            <h1>{name}</h1>
+            <h2>
+              {console.log(location)}
+              {animal} - {breed} - {location}
+            </h2>
+          </div>
+        </div>
+      )
+  }
+}
