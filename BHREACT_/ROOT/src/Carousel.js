5ae069ab9c22f5eb4cc01d5dc253b70098bc80aa
[Andres] <andresar11@gmail.com>
2018-11-06 16:29:12 -0500

    Added interactivity to Carousel component

--- BHREACT_/ROOT/src/Carousel.js
+++ BHREACT_/ROOT/src/Carousel.js
@@ -15,6 +15,13 @@ export default class Carousel extends React.Component {
 
     return { photos }
   }
+//add + in order to cast data type so it can work with img classname ternary operator
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    })
+  }
+
   render() {
     const { photos, active } = this.state
 
@@ -23,7 +30,10 @@ export default class Carousel extends React.Component {
         <img src={photos[active].value} alt="primary animal"/>
         <div className="carousel-smaller">
           {photos.map((photo, index) => (
+            /* eslint-disable-next-line */
             <img
+              onClick={this.handleIndexClick}
+              data-index = {index}
               key={photo.value}
               src={photo.value}
               className={index === active ? 'active' : ''}
