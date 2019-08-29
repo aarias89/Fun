8d6aef0212a236ca3f9d422582abf80792f78e14
Andres-Dev <andres-dev@Nicoles-MacBook-Pro.local>
2019-08-28 21:00:44 -0400

    Add code for initializing graphQL

--- ReactGraphQL/GraphQLMock/users/schema/schema.js
+++ ReactGraphQL/GraphQLMock/users/schema/schema.js
@@ -1,5 +1,6 @@
-import { GraphQLObjectType, GraphQLString, GraphQLInt } from 'graphql';
-// const { GraphQLObjectType } = graphql;
+const graphql = require('graphql');
+const _ = require('lodash');
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -8,4 +9,28 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+});
+
+// hardcoded mock data
+const users = [
+  { id: '23', firstName: 'Bilbo', age: '430' },
+  { id: '46', firstName: 'Frodo', age: '32' },
+  { id: '54', firstName: 'Boromir', age: '45' }
+];
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 });
